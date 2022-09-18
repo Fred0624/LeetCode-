3cbd6713105ba2eee0bd930b56f8b598316bb671
Fred0624 <fred10150912@hotmail.com>
2022-09-18 16:13:20 -0700

    342-power-of-four

---
+++ solutions/342-power-of-four.js
@@ -0,0 +1,9 @@
+// Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+// An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+// n = 4^x ==> log4(n) = x, if x is an integer, so it is true
+
+var isPowerOfFour = function(n) {
+    return Number.isInteger(Math.log(n) / Math.log(4))
+};
