03044b1d302f739f0b8d46e9a1a4db71e256001b
Fred0624 <fred10150912@hotmail.com>
2022-09-17 18:24:17 -0700

    2160-minimum-sum-of-four-digit

---
+++ solutions/2160-minimum-sum-of-four-digit.js
@@ -0,0 +1,11 @@
+// You are given a positive integer num consisting of exactly four digits. Split num into two new integers new1 and new2 by using the digits found in num. Leading zeros are allowed in new1 and new2, and all the digits found in num must be used.
+
+// For example, given num = 2932, you have the following digits: two 2's, one 9 and one 3. Some of the possible pairs [new1, new2] are [22, 93], [23, 92], [223, 9] and [2, 329].
+// Return the minimum possible sum of new1 and new2.
+
+var minimumSum = function(num) {
+    let arr = num.toString().split('').sort()
+    let num1 = arr[0] + arr[2]
+    let num2 = arr[1] + arr[3]
+    return parseInt(num1) + parseInt(num2)
+};
