a1a9895ada10a3213435123bb500fd43ec8bb274
Fred0624 <fred10150912@hotmail.com>
2022-11-02 23:27:30 -0700

    1523-count-odd-numbers-in-an-interval-range

--- solutions/1523-count-odd-numbers-in-an-interval-range.js
+++ solutions/1523-count-odd-numbers-in-an-interval-range.js
@@ -16,7 +16,7 @@ var countOdds = function(low, high) {
     //even odd : 4 13  -> 5,7,9,11,13 - 5: Math.floor(diff/2)+1
     //odd even : 3 12  ->3,5,7,9,11  - 5 : floor(diff/2)+1 - same a s prev
     //both odd : 3 13  -> 3,5,7,9,11,13- 6:floor(diff/2)+1 -same
-    //0 13. -> 1,3,5,7,9,11,13 - 7  test case
+    //0 13. -> 1,3,5,7,9,11,13 - 7  test case 
     if(low%2 === 0 && high%2 === 0){
         return (high - low)/2
     } else{
