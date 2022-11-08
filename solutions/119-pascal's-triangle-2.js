90ab847bec337f56e519392a6ce175828395de5a
Fred0624 <fred10150912@hotmail.com>
2022-11-07 16:03:02 -0800

    119-pascal's-triangle-2

--- solutions/119-pascal's-triangle-2.js
+++ solutions/119-pascal's-triangle-2.js
@@ -1,23 +1,24 @@
-// Given an integer numRows, return the first numRows of Pascal's triangle.
+// Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
 
 // In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
 
 
 
 // Example 1:
-// Input: numRows = 5
-// Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+// Input: rowIndex = 3
+// Output: [1,3,3,1]
 
 // Example 2:
-// Input: numRows = 1
-// Output: [[1]]
+// Input: rowIndex = 0
+// Output: [1]
 
-var generate = function(numRows) {
-    if(numRows === 0){
-        return []
-    }
+// Example 3:
+// Input: rowIndex = 1
+// Output: [1,1]
+
+var getRow = function(rowIndex) {
     let res = []
-    for(let i = 0; i < numRows; i++){
+    for(let i = 0; i <= rowIndex; i++){
         let cur = []
         for(let j = 0; j <= i; j++){
             if(j === 0 || j === i){
@@ -28,5 +29,5 @@ var generate = function(numRows) {
         }
         res.push(cur)
     }
-    return res
+    return res[rowIndex]
 };
